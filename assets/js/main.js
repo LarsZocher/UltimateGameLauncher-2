f4d55e56b9f411f5d686644cde2b30f53051fca1
bubbleegg <bubbleegg@freenet.de>
2019-09-22 17:57:49 +0200

    Added files

---
+++ assets/js/main.js
@@ -0,0 +1,117 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const { app, BrowserWindow, Menu, ipcMain} = electron;
+
+let mainWindow;
+let addWindow;
+
+//Listen for the app to be ready
+app.on('ready', function () {
+    //Create new window
+    mainWindow = new BrowserWindow({
+        webPreferences:{
+            nodeIntegration: true
+        },
+        width: 800,
+        height: 600,
+        frame: false
+    });
+    console.log(__dirname);
+    //Load html
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, '../pages/mainWindow.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+    mainWindow.on('closed', function () {
+        app.quit();
+    })
+
+    //Build menu from template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+});
+
+//Handle add window
+function createAddWindow() {
+    //Create new window
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Eintrag hinzufügen',
+        webPreferences:{
+            nodeIntegration: true
+        }
+    });
+    //Load html
+    addWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'addWindow.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+    addWindow.on('close', function () {
+        addWindow = null;
+    })
+}
+
+//Catch Item add
+ipcMain.on('item:add', function(e, item){
+    mainWindow.webContents.send('item:add', item);
+    addWindow.close();
+});
+
+//Create menu template
+const mainMenuTemplate = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Eintrag hinzufügen',
+                click() {
+                    createAddWindow();
+                }
+            },
+            {
+                label: 'Lösche Einträge',
+                click(){
+                    mainWindow.webContents.send('item:clear')
+                }
+            },
+            {
+                label: 'Beenden',
+                accelerator: process.platform == 'darwin' ? 'Command+Q' : 'CTRL+Q',
+                click() {
+                    app.quit();
+                }
+            },
+        ]
+    }
+];
+
+//If mac, add empty obj
+if (process.platform == 'darwin') {
+    mainMenuTemplate.unshift({});
+}
+
+//Add dev tools
+if (process.env.NODE_ENV !== 'production') {
+    mainMenuTemplate.push({
+        label: 'Entwickler',
+        submenu: [
+            {
+                label: 'Toggle DevTools',
+                accelerator: process.platform == 'darwin' ? 'Command+I' : 'CTRL+I',
+                click(item, focusedWindow){
+                    focusedWindow.toggleDevTools();
+                }
+            },
+            {
+                role: 'reload'
+            }
+        ]
+    });
+}
---
+++ assets/js/steam.js
@@ -0,0 +1,132 @@
+
+
+function getAppsById(cb, ids){
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var result = JSON.parse(this.responseText);
+            cb(result);
+        }
+    };
+    for (let i = 0; i < ids.length; i++) {
+        ids[i] = "STEAM_"+ids[i];
+        
+    }
+    xmlhttp.open("GET", "https://ugl.seemslegit.me/getGames?uniqueID=" + ids.join(","), true);
+    xmlhttp.send();
+}
+
+function getAppsFromUser(cb, userId, onlyGames = true){
+    console.log(`loading games from user ${userId}`);
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var result = JSON.parse(this.responseText);
+            console.log(result);
+            cb(result);
+        }
+    };
+    xmlhttp.open("GET", "https://ugl.seemslegit.me/getGamesFromUser?id=" + userId, true);
+    xmlhttp.send();
+}
+
+function openApp(appid, dev = false){
+    var executablePath = "\"D:\\Program Files (x86)\\Steam\\Steam.exe\" -applaunch "+appid;
+    const { exec } = require('child_process');
+    exec(executablePath, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+    });
+}
+
+async function isRunning(){
+    var executablePath = "tasklist";
+    const util = require('util');
+    const exec = util.promisify(require('child_process').exec);
+    const { stdout, stderr } = await exec(executablePath);
+    return stdout.includes("Steam.exe");
+}
+
+async function isLoggedIn(){
+    var Registry = require('winreg')
+,   regKey = new Registry({
+      hive: Registry.HKCU,
+      key:  '\\Software\\Valve\\Steam\\ActiveProcess'
+    })
+
+    let p = new Promise((res, rej) => {
+        regKey.values((err, items) => {
+            for (var i=0; i<items.length; i++){
+                if(items[i].name == "ActiveUser"){
+                    res(items[i].value != 0x0);
+                }
+            }
+        });
+    });
+    return await p;
+}
+
+
+async function getLastUser(){
+    var Registry = require('winreg')
+,   regKey = new Registry({
+      hive: Registry.HKCU,
+      key:  '\\Software\\Valve\\Steam'
+    })
+
+    let p = new Promise((res, rej) => {
+        regKey.values(function (err, items) {
+            for (var i=0; i<items.length; i++){
+                if(items[i].name == "AutoLoginUser"){
+                    res(items[i].value);
+                }
+            }
+        });
+    });
+    return await p;
+}
+
+async function getCurrentUser(){
+    var li = await isLoggedIn();
+    if(li){
+        return await getLastUser();
+    }
+    return "";
+}
+
+
+async function changeUser(user){
+    var running = await isRunning();
+    var cUser = await getCurrentUser();
+    if(running && cUser!=user["name"]){
+        var executablePath = "taskkill /F /IM steam.exe";
+        const { exec } = require('child_process');
+        exec(executablePath, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`exec error: ${error}`);
+                return;
+            }
+        });
+        await timeout(1000);
+    }
+
+    console.log("Starting steam client - "+user["name"]);
+    var executablePath = "\"D:\\Program Files (x86)\\Steam\\Steam.exe\" -login "+user["name"] + " " + user["pass"];
+    const { exec } = require('child_process');
+    exec(executablePath, (error, stdout, stderr) => {
+        
+    });
+}
+
+async function timeout(time){
+    let p = new Promise((res, rej) => {
+        setTimeout(()=>{
+            res();
+        }, time);
+    });
+    await p;
+}
