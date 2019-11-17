db4e46b543dcb9ce77defc0807a960654685c07b
bubbleegg <bubbleegg@freenet.de>
2019-11-17 10:23:14 +0100

    React test

---
+++ assets/js/components/Test.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class Test extends Component {
+    render() {
+        return (
+            <div style="color: #FFF;">Hey was geht</div>
+        );
+    }
+}
---
+++ assets/js/react.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+console.log("test");
+
+ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById('content'));
+console.log("test");
